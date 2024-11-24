8f6f604d0ec5111b3c3b5033847189fb059a7519
NagarajuArnepalli <raju13401@gmail.com>
2024-11-25 00:52:31 +0530

    Create readme.sh
    New public file is created

---
+++ readme.sh
@@ -0,0 +1,7 @@
+echo "# PublicRepository" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/NagarajuArnepalli/PublicRepository.git
+git push -u origin main
